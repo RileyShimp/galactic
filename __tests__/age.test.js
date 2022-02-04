b2385c224ffb63e5755c666eb2dedd268038e407
Riley Shimp <rilryshimp@gmail.com>
2022-02-04 10:18:08 -0800

    Add code for sixthtest of Age constructor

--- __tests__/age.test.js
+++ __tests__/age.test.js
@@ -22,13 +22,21 @@ describe('age', () => {
 
   test('should correctly determine venus age based on age entered', () => {
     expect(age.venusCalc()).toEqual(32);
-  })
+  });
 
   test('should correctly determine mars age based on age entered', () => {
     expect(age.marsCalc()).toEqual(10);
-  })
+  });
 
   test('should correctly determine jupiter age based on age entered', () => {
     expect(age.jupiterCalc()).toEqual(1);
-  })
+  });
+
+  test('should correctly provide a life expectancy for each planet', () => {
+    expect(age.earthExpect).toEqual(72);
+    expect(age.mercuryExpect).toEqual(0);
+    expect(age.venusExpect).toEqual(0);
+    expect(age.marsExpect).toEqual(0);
+    expect(age.jupiterExpect).toEqual(0);
+  });
 });
